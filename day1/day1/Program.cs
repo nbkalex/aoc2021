f3a0832df95a7cf179519cfea2ff4f91782636bb
alex <alex.ioan.dragomir@gmail.com>
2021-12-01 10:11:32 +0200

    day1

---
+++ day1/day1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllText("input.txt").Split("\r\n").Select(l => int.Parse(l));
+            Console.WriteLine("part1: " + input.Skip(1).Zip(input).Count(z => z.First > z.Second));
+
+            var input3 = input.Skip(2).Zip(input.Skip(1).Zip(input)).Select(z => z.First + z.Second.First + z.Second.Second);
+            Console.WriteLine("part2: " + input3.Skip(1).Zip(input3).Count(z => z.First > z.Second));
+        }
+    }
+}
