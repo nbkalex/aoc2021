f0a4296e2e2173effc06071e3ace16e8971f73cb
alex <alex.ioan.dragomir@gmail.com>
2021-12-01 15:50:12 +0200

    Day1 part2 reworked

--- day1/day1/Program.cs
+++ day1/day1/Program.cs
@@ -8,11 +8,9 @@ namespace day1
     {
         static void Main(string[] args)
         {
-            var input = File.ReadAllText("input.txt").Split("\r\n").Select(l => int.Parse(l));
+            var input = File.ReadAllText("input.txt").Split("\r\n").Select(l => int.Parse(l)).ToArray();
             Console.WriteLine("part1: " + input.Skip(1).Zip(input).Count(z => z.First > z.Second));
-
-            var input3 = input.Skip(2).Zip(input.Skip(1).Zip(input)).Select(z => z.First + z.Second.First + z.Second.Second);
-            Console.WriteLine("part2: " + input3.Skip(1).Zip(input3).Count(z => z.First > z.Second));
+            Console.WriteLine("part2: " + input.Skip(3).Zip(input).Count(z => z.First > z.Second));
         }
     }
 }
