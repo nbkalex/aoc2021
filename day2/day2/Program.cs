f35d865f0931293aa9cde202eabc1891333632e4
alex <alex.ioan.dragomir@gmail.com>
2021-12-02 07:31:41 +0200

    day2

---
+++ day2/day2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = File.ReadAllText("input.txt")
+                .Split("\r\n");
+
+int currentH = 0;
+int currentD = 0;
+int currentA = 0;
+
+foreach (var line in input)
+{
+    int val = int.Parse(line.Split(" ")[1]);
+    if (line.StartsWith("f"))
+    { 
+        currentH += val;
+        currentD += val * currentA;
+    }
+    if (line.StartsWith("u"))
+        currentA -= val;
+    if (line.StartsWith("d"))
+        currentA += val;
+}
+
+Console.WriteLine(currentH *  currentD);
