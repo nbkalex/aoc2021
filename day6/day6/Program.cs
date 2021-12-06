cc408fd25b360345b69458453b1ee29a1098c055
Alex Dragomir <alex.dragomir@caphyon.com>
2021-12-06 14:18:26 +0200

    day6 more readable

--- day6/day6/Program.cs
+++ day6/day6/Program.cs
@@ -11,26 +11,15 @@ namespace day6
         {
             var input = File.ReadAllLines("input.txt")[0].Split(",").Select(l => int.Parse(l)).ToList();
             long days = 256;
-
-            var numbers = new Dictionary<long, long>();
-            for (int i = 0; i <= 8; i++)
-                numbers.Add(i, 0);
-
+            var numbers = Enumerable.Range(0, 9).ToDictionary(n => (long)n, n => (long)0);
             foreach (var i in input)
                 numbers[i]++;
 
             for (int i = 0; i < days; i++)
             {
                 long zeros = numbers[0];
-
-                long lastValue = numbers[8];
-                for (int j = 7; j >= 0; j--)
-                {
-                    var current = numbers[j];
-                    numbers[j] = lastValue;
-                    lastValue = current;
-                }
-
+                for (int j = 0; j < 8 ; j++)
+                    numbers[j] = numbers[j+1];
                 numbers[6] += zeros;
                 numbers[8] = zeros;
             }
