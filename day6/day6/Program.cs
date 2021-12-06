ff5d66c8bc8c36140c6cc12c48ed0dbad5502831
Alex Dragomir <alex.dragomir@caphyon.com>
2021-12-06 15:08:05 +0200

    day6 best

--- day6/day6/Program.cs
+++ day6/day6/Program.cs
@@ -9,10 +9,13 @@ namespace day6
     {
         static void Main(string[] args)
         {
-            int days = 256;
-            var input = File.ReadAllLines("input.txt")[0].Split(",").Select(l => int.Parse(l));
-            var numbers = Enumerable.Range(0, 9).ToDictionary(n => (long)n, n => (long)input.Count(i => i == n));
-            Enumerable.Range(0, days+1).Aggregate((_,__) =>
+            var input = File.ReadAllLines("input.txt")[0].Split(",").Select(l => int.Parse(l)).ToList();
+            long days = 256;
+            var numbers = new long[9];
+            foreach (var i in input)
+                numbers[i]++;
+
+            for (int i = 0; i < days; i++)
             {
                 long zeros = numbers[0];
                 for (int j = 0; j < 8; j++)
@@ -23,7 +26,7 @@ namespace day6
                 return 0;
             });
 
-            Console.WriteLine(numbers.Values.Sum());
+            Console.WriteLine(numbers.Sum());
         }
     }
 }
