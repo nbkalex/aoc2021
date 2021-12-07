6abdf8e58fc74bdb533cf7ea910dc41a58377b68
Alex Dragomir <alex.dragomir@caphyon.com>
2021-12-07 11:42:11 +0200

    day7

---
+++ day7/day7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers =  File.ReadAllText("input.txt").Split(",").Select(n => int.Parse(n));
+            Console.WriteLine("Part 1: " + Enumerable.Range(0, numbers.Max()).ToDictionary(n => n, n => numbers.Aggregate(0, (seed, n1) => seed + Math.Abs(n1 - n))).Min(dif => dif.Value));
+            Console.WriteLine("Part 2: " + Enumerable.Range(0, numbers.Max()).ToDictionary(n => n, n => numbers.Aggregate(0, (seed, n1) => seed + (Math.Abs(n1 - n) * (Math.Abs(n1 - n)+1)/2))).Min(dif => dif.Value));
+        }
+    }
+}
