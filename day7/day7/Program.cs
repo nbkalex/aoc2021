117880384893332ff0382448fb96b920005dceb4
Alex Dragomir <alex.dragomir@caphyon.com>
2021-12-07 12:03:06 +0200

    day7 simplified

--- day7/day7/Program.cs
+++ day7/day7/Program.cs
@@ -9,8 +9,8 @@ namespace day7
         static void Main(string[] args)
         {
             var numbers =  File.ReadAllText("input.txt").Split(",").Select(n => int.Parse(n));
-            Console.WriteLine("Part 1: " + Enumerable.Range(0, numbers.Max()).ToDictionary(n => n, n => numbers.Aggregate(0, (seed, n1) => seed + Math.Abs(n1 - n))).Min(dif => dif.Value));
-            Console.WriteLine("Part 2: " + Enumerable.Range(0, numbers.Max()).ToDictionary(n => n, n => numbers.Aggregate(0, (seed, n1) => seed + (Math.Abs(n1 - n) * (Math.Abs(n1 - n)+1)/2))).Min(dif => dif.Value));
+            Console.WriteLine("Part 1: " + Enumerable.Range(0, numbers.Max()).Select(n => numbers.Aggregate(0, (acc, n1) => acc + Math.Abs(n1 - n))).Min());
+            Console.WriteLine("Part 2: " + Enumerable.Range(0, numbers.Max()).Select(n => numbers.Aggregate(0, (acc, n1) => acc + (Math.Abs(n1 - n) * (Math.Abs(n1 - n)+1)/2))).Min());
         }
     }
 }
