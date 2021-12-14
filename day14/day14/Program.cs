d13142c21fd9367eae1303763af471ee7cdf41e9
alex <alex.ioan.dragomir@gmail.com>
2021-12-14 10:13:06 +0200

    day 14 2

--- day14/day14/Program.cs
+++ day14/day14/Program.cs
@@ -18,62 +18,25 @@ var freqPairs = pairs.ToDictionary(kvp => kvp.Key, kvp => template.IndexOf(kvp.K
 
 foreach (var step in Enumerable.Range(0, 10))
 {
-    //Console.WriteLine("----------------------");
-
-    //var freqPairsCopy = freqPairs.ToDictionary(f => f.Key, f => f.Value);
-    Console.WriteLine(freqPairs.Values.Sum());
+    var freqPairsCopy = freqPairs.ToDictionary(f => f.Key, f => f.Value);
+    
     Dictionary<string, long> newFreq = freqPairs.ToDictionary(f => f.Key, f => (long)0);
     foreach (var pair in pairs)
     {
-        long current = freqPairs[pair.Key];
+        long current = freqPairsCopy[pair.Key];
         if (current == 0)
             continue;
+
+        freqPairs[pair.Key] = 0;
 
         newFreq[pair.Key[0] + pair.Value] += current;
         newFreq[pair.Value + pair.Key[1]] += current;
     }
 
     foreach (var pair in pairs)
-    { 
-        if(newFreq[pair.Key] > 0)
-            freqPairs[pair.Key] = newFreq[pair.Key];
-    }
+      freqPairs[pair.Key] = newFreq[pair.Key];
 
-    //    var checkSize = freqPairs.Values.Sum();
-
-    //List<(string, int)> pairsFound = new List<(string, int)>();
-
-    //foreach (var pair in pairs)
-    //{
-    //    int offSet = 0;
-    //    while (true)
-    //    {
-    //        int indexFound = template.IndexOf(pair.Key, offSet);
-    //        if (indexFound == -1)
-    //            break;
-
-    //        pairsFound.Add((pair.Value, ++indexFound));
-
-    //        offSet = indexFound;
-    //    }
-    //}
-
-    //int offset = 0;
-    //foreach (var pair in pairsFound.OrderBy(p => p.Item2))
-    //{
-    //    template = template.Insert(offset + pair.Item2, pair.Item1);
-    //    offset++;
-    //}
-
-    //Console.WriteLine();
-
-    //if(tests.ContainsKey(step))  
-    //    Debug.Assert(template == tests[step]);
-
-    //Console.WriteLine(template);
-
-    //var freqs = template.Distinct().ToDictionary(c => c, c => template.Count(c2 => c == c2));
-    //Console.WriteLine(freqs.Values.Max().ToString() + "-" + freqs.Values.Min());
+    var count = freqPairs.Values.Sum();
 }
 
 var allElements = freqPairs.Keys.Aggregate("", (acc, k) => acc + k).ToHashSet().ToDictionary(c => c, c => (long)0);
@@ -84,5 +47,4 @@ foreach (var pair in freqPairs)
 }
 
 
-var freqs2 = template.Distinct().ToDictionary(c => c, c => template.Count(c2 => c == c2));
 Console.WriteLine(allElements.Values.Max() - allElements.Values.Min());
