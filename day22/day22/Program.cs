07a090aed10c9974678c06c528deb348def12d2d
alex <alex.ioan.dragomir@gmail.com>
2021-12-23 02:27:08 +0200

    day22 part2 wip

--- day22/day22/Program.cs
+++ day22/day22/Program.cs
@@ -12,8 +12,9 @@ foreach (var line in input)
   cubes.Add((on, xs, ys, zs));
 }
 
-Dictionary<(int,int,int), bool> cubesFound = new Dictionary<(int, int, int), bool>();
-
+Dictionary<(int, int, int), bool> cubesFound = new Dictionary<(int, int, int), bool>();
+var initialized = new List<(bool, (int, int), (int, int), (int, int))>();
+long total = 0;
 foreach (var cub in cubes)
 {
   int x1 = cub.Item2.Item1;
@@ -29,16 +30,102 @@ foreach (var cub in cubes)
    && y1 >= -50 && y2 <= 50
    && z1 >= -50 && z2 <= 50)
   {
-    for (int i = x1; i <= x2; i++)
-      for (int j = y1; j <= y2; j++)
-        for (int k = z1; k <= z2; k++)
-        {
-          if(cubesFound.ContainsKey((i,j,k)))
-            cubesFound[(i,j,k)] = cub.Item1;
-          else
-            cubesFound.Add((i,j,k), cub.Item1);
-        }
+    long vol = GetVol(cub);
+    if(cub.Item1)
+      total += vol;
+
+    foreach (var initCub in initialized)
+    {
+      var intersection = GetIntersection(cub, initCub);
+      total-= GetVol(intersection);
+    }
+
+    initialized.Add(cub);
   }
 }
 
-Console.WriteLine(cubesFound.Values.Count(v => v));
+
+Console.WriteLine(total);
+
+
+long GetVol((bool, (int, int), (int, int), (int, int)) cub)
+{
+  int x1 = cub.Item2.Item1;
+  int x2 = cub.Item2.Item2;
+
+  int y1 = cub.Item3.Item1;
+  int y2 = cub.Item3.Item2;
+
+  int z1 = cub.Item4.Item1;
+  int z2 = cub.Item4.Item2;
+
+  return Math.Abs(x2 - x1) * Math.Abs(y2 - y1) * Math.Abs(z2 - z1);
+}
+
+(bool, (int, int), (int, int), (int, int)) GetIntersection((bool, (int, int), (int, int), (int, int)) cub1, (bool, (int, int), (int, int), (int, int)) cub2)
+{
+  int x11 = cub1.Item2.Item1;
+  int x12 = cub1.Item2.Item2;
+
+  int y11 = cub1.Item3.Item1;
+  int y12 = cub1.Item3.Item2;
+
+  int z11 = cub1.Item4.Item1;
+  int z12 = cub1.Item4.Item2;
+
+  int x21 = cub2.Item2.Item1;
+  int x22 = cub2.Item2.Item2;
+
+  int y21 = cub2.Item3.Item1;
+  int y22 = cub2.Item3.Item2;
+
+  int z21 = cub2.Item4.Item1;
+  int z22 = cub2.Item4.Item2;
+
+  int x1Dif = 0;
+  int x2Dif = 0;
+
+  if (x11 <= x21 && x12 >= x21)
+  {
+    x1Dif = x21;
+    x2Dif = Math.Min(x12, x22);
+  }
+
+  if (x11 <= x22 && x11 >= x21)
+  {
+    x1Dif = x11;
+    x2Dif = Math.Min(x12, x22);
+  }
+
+  int y1Dif = 0;
+  int y2Dif = 0;
+
+  if (y11 <= y21 && y12 >= y21)
+  {
+    y1Dif = y21;
+    y2Dif = Math.Min(y12, y22);
+  }
+
+  if (y11 <= y22 && y11 >= y21)
+  {
+    y1Dif = y11;
+    y2Dif = Math.Min(y12, y22);
+  }
+
+  int z1Dif = 0;
+  int z2Dif = 0;
+
+  if (z11 <= z21 && z12 >= z21)
+  {
+    z1Dif = z21;
+    z2Dif = Math.Min(z12, z22);
+  }
+
+  if (z11 <= z22 && z11 >= z21)
+  {
+    z1Dif = z11;
+    z2Dif = Math.Min(z12, z22);
+  }
+
+  return (cub2.Item1, (x1Dif, x2Dif), (y1Dif, y1Dif), (z1Dif, z1Dif));
+}
