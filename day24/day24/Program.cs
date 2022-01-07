316c070d4ce1f4858581df92f0033cc413ee9501
Alex Dragomir <alex.dragomir@caphyon.com>
2022-01-07 12:41:39 +0200

    day 24

--- day24/day24/Program.cs
+++ day24/day24/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System.Numerics;
 
-int index = 0;
-
 var program = File.ReadAllLines("input.txt").Select(l =>
 {
   string[] tokens = l.Split(" ");
@@ -9,34 +7,9 @@ var program = File.ReadAllLines("input.txt").Select(l =>
   return (tokens[0], tokens[1], secondArg);
 });
 
-//var output = ProcessInput(new Stack<int>(new List<int>() { 7 }));
-HashSet<BigInteger> results = new HashSet<BigInteger>();
-
-BigInteger count = 0;
-BigInteger min = 99999999999999;
-BigInteger max = 0;
-
-for (BigInteger i = 99999999999999; i > 9999999999999; i-= 1)
-{
-  if(i.ToString().Contains('0'))
-    continue;
-
-  //Console.Clear();
-  var output = ProcessNumber(i);
-
-  if (min >= output.Item4)
-  {
-    Console.WriteLine(i.ToString() + " : " + min.ToString() + " -> " + output.Item4.ToString());
-    min = output.Item4;
-  }
-
-  //if (max < output.Item4)
-  //{
-  //  Console.WriteLine(i.ToString() + " : " + max.ToString() + " -> " + output.Item4.ToString());
-  //  max = output.Item4;
-  //}
-}
-
+BigInteger maxNumber = 36969794979199;
+BigInteger minNumber = 11419161313147;
+ProcessNumber(minNumber);
 
 (BigInteger, BigInteger, BigInteger, BigInteger) ProcessNumber(BigInteger number)
 {
@@ -64,21 +37,23 @@ for (BigInteger i = 99999999999999; i > 9999999999999; i-= 1)
     {"eql", (a,b) => a==b ? 1 : 0}
   };
 
-  int count = 0;
+  int i = 1;
   foreach (var instruction in program)
   {
-    count++;
-    if(count == 241)
-    { 
-    }
-
     BigInteger valueArg2 = 0;
     if (instruction.Item3 != "" && !BigInteger.TryParse(instruction.Item3, out valueArg2))
       valueArg2 = variables[instruction.Item3];
     variables[instruction.Item2] = instructions[instruction.Item1](variables[instruction.Item2], valueArg2);
 
-    //Console.WriteLine($"{instruction.Item1} {instruction.Item2} {instruction.Item3} => " + (variables["w"], variables["x"], variables["y"], variables["z"]));
+    if (instruction.Item1.StartsWith("inp"))
+    {
+      Console.WriteLine(" -> " + variables["z"]);
+      Console.Write(i.ToString() + ": " + variables["w"]);
+      i++;
+    }
   }
+
+  Console.WriteLine(" -> " + variables["z"]);
 
   return (variables["w"], variables["x"], variables["y"], variables["z"]);
 }
